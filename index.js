3cd025f5fec4b7c5cc2fe73bde5cb2e5df403fde
Islam Gad <i_gad_@hotmail.com>
2020-03-09 17:00:29 +0200

    Remove useless required

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 "use-strict";
 require("dotenv").config();
-const { exec } = require("child_process");
 const { cfPush } = require("./cf-push");
-const endpoint = process.env.ENDPOINT;
-const username = process.env.USER_NAME;
-const password = process.env.PASSWORD;
-
 const lowCodeAppGUID = "41d24d5f-0761-4557-b754-56a1238de546";
 const developmentSpaceGUID = "e9977f50-ca20-4dff-a049-e8626c44bfe8";
 const lowCodeAppName = "low-code-app";
